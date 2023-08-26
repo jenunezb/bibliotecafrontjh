a62c92a86629c2a97305083d57b03b2ee17e6fc3
seacasta <seacasta@bancolombia.com.co>
2023-08-26 18:39:59 -0500

    Adecuando login más intermedio de página

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './pagina/login/login.component';
+import { MenuComponent } from './pagina/menu/menu.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagina/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading = true;
+
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+  ngOnInit(): void {
+
+  }
+  ingresar() {
+
+    const email = this.form.value.email;
+    const password = this.form.value.password;
+
+    console.log(email);
+    console.log(password);
+
+
+    if (email == 'proyectogrado@gmail.com' && password == 'holamundo') {
+      //lo llevamos al menu
+    } else {
+      //mostramos un mensaje
+      this.error();
+    }
+  }
+
+  error() {
+    this._snackBar.open('Email o contraseña invalido', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    })
+  }
+}
+
+
+
+
